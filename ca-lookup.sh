1af57338d9dc4e7747d23e6ca90769494de27291
Ashley Cawley <ash@ashleycawley.co.uk>
2020-09-10 11:21:34 +0100

    Facelift on how data is laid out

--- ca-lookup.sh
+++ ca-lookup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Variables
+# Author: Ashley Cawley // @ashleycawley
+
+# Variables & Functions
+VERSION="1.0.3"
 DOMAIN=$(echo $1 | sed 's,http://,,g' | sed 's,https://,,g' | sed 's,/,,g' | sed 's,www.,,g' )
 EXPIRY_DATE=$(whois $DOMAIN | grep -i "expiry date:" | sed -e 's/   Registry Expiry Date: //g' | sed -e 's/        Expiry date:  //g')
 A_RECORD=$(dig $DOMAIN A +short)
 RDNS_HOSTNAME=$(dig -x $A_RECORD +short)
 WWW_RECORD=$(dig www.$DOMAIN A +short | grep -v $DOMAIN)
+MAIL_RECORD=$(dig mail.$DOMAIN A +short | grep -v $DOMAIN)
 MX_RECORD=$(dig $DOMAIN MX +short)
+TXT_RECORD=$(dig $DOMAIN TXT +short)
+
 # Colours
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
@@ -14,13 +20,28 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo "
-Probing Domain: $DOMAIN (Expiry: $EXPIRY_DATE)
+ca-lookup Version: $VERSION | Source code available at: http://swb.me/calookup
 
-Server		:	$RDNS_HOSTNAME
-A		:	$DOMAIN - $A_RECORD
-WWW		:	www.$DOMAIN - $WWW_RECORD
-MX		:	$MX_RECORD
+ --------------------------------------------------------------------------
+| Domain  :   $DOMAIN expires on $EXPIRY_DATE
+ -------------------------------------------------------------------------
+| Server  :   $RDNS_HOSTNAME
+ --------------------------------------------------------------------------
+| A       :   $DOMAIN - $A_RECORD
+ -------------------------------------------------------------------------
+| WWW.    :   $WWW_RECORD
+ -------------------------------------------------------------------------
+| MAIL.   :   $MAIL_RECORD
+ -------------------------------------------------------------------------
+| MX Record(s)
+$MX_RECORD
+ -------------------------------------------------------------------------
+| TXT Record(s):
+$TXT_RECORD
+ -------------------------------------------------------------------------
 "
+
+echo -e "Connectivity Check: \c"
 ping -c 1 "$DOMAIN" &>/dev/null
 PING_STATUS=$(echo $?)
 
@@ -29,6 +50,9 @@ then
     echo -e ${GREEN}$DOMAIN is responding to ping.${NC} && echo
 else
     echo -e ${YELLOW}$DOMAIN is not responding to ping.${NC} && echo
+    echo "Do not worry if the server is not responding to ping, some servers may block ping (ICMP) traffic"
+    echo "but may still have their web ports (80 or 443) open and accessible."
+    echo ""
     exit 1
 fi
 
