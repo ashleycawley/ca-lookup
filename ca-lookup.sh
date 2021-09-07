9fbfd3506d19631383005cb8882a8a11d924ae61
Ashley Cawley <ash@ashleycawley.co.uk>
2021-09-07 14:54:32 +0100

    v1.5.0 Added Server resolution even if domain uses CloudFlare NS

--- ca-lookup.sh
+++ ca-lookup.sh
@@ -49,14 +49,25 @@ then
 fi
 
 # Variables & Functions
-VERSION="1.3.0"
+VERSION="1.5.0"
 DOMAIN=$(echo $1 | sed 's,http://,,g' | sed 's,https://,,g' | sed 's,/,,g' | sed 's,www.,,g' )
 LOOKING_FOR_NS=$(whois -H $DOMAIN)
 COM_NS_CHECK=$(echo "$LOOKING_FOR_NS" | grep "^Name Server:" | sed 's/Name Server: //g')
 CO_UK_NS_CHECK=$(echo "$LOOKING_FOR_NS" | grep -A 2 "Name servers:" | sed '/Name servers:/d' | sed 's/^ *//g' | awk '{print $1}')
-EXPIRY_DATE=$(whois $DOMAIN | grep -i "expiry date:" | sed -e 's/   Registry Expiry Date: //g' | sed -e 's/        Expiry date:  //g')
 A_RECORD=$(dig $DOMAIN A +short)
-RDNS_HOSTNAME=$(dig -x $A_RECORD +short)
+
+# CloudFlare Check
+echo "$COM_NS_CHECK" | grep -i "cloudflare" &>/dev/null && CLOUDFLARE_CHECK="1"
+echo "$CO_UK_NS_CHECK" | grep -i "cloudflare" &>/dev/null && CLOUDFLARE_CHECK="1"
+if [ "$CLOUDFLARE_CHECK" == "1" ]
+then
+    IP_AT_CLOUDABOVE=$(dig A $DOMAIN +short @ns1.cloudabove.com)
+    RDNS_HOSTNAME=$(dig -x $IP_AT_CLOUDABOVE +short)
+else
+    RDNS_HOSTNAME=$(dig -x $A_RECORD +short)
+fi
+
+EXPIRY_DATE=$(whois $DOMAIN | grep -i "expiry date:" | sed -e 's/   Registry Expiry Date: //g' | sed -e 's/        Expiry date:  //g')
 WWW_RECORD=$(dig www.$DOMAIN A +short | grep -v $DOMAIN)
 MAIL_RECORD=$(dig mail.$DOMAIN A +short | grep -v $DOMAIN)
 MX_RECORD=$(dig $DOMAIN MX +short)
