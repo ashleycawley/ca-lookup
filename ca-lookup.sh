15bd070e7f4a7f18ed508951c4c9fab12fafeaae
Ashley Cawley <ash@ashleycawley.co.uk>
2021-09-07 15:39:23 +0100

    Added better error handling

--- ca-lookup.sh
+++ ca-lookup.sh
@@ -49,12 +49,18 @@ then
 fi
 
 # Variables & Functions
-VERSION="1.5.0"
+VERSION="1.5.1"
 DOMAIN=$(echo $1 | sed 's,http://,,g' | sed 's,https://,,g' | sed 's,/,,g' | sed 's,www.,,g' )
 LOOKING_FOR_NS=$(whois -H $DOMAIN)
 COM_NS_CHECK=$(echo "$LOOKING_FOR_NS" | grep "^Name Server:" | sed 's/Name Server: //g')
 CO_UK_NS_CHECK=$(echo "$LOOKING_FOR_NS" | grep -A 2 "Name servers:" | sed '/Name servers:/d' | sed 's/^ *//g' | awk '{print $1}')
 A_RECORD=$(dig $DOMAIN A +short)
+
+# Colours
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 # CloudFlare Check
 echo "$COM_NS_CHECK" | grep -i "cloudflare" &>/dev/null && CLOUDFLARE_CHECK="1"
@@ -62,7 +68,8 @@ echo "$CO_UK_NS_CHECK" | grep -i "cloudflare" &>/dev/null && CLOUDFLARE_CHECK="1
 if [ "$CLOUDFLARE_CHECK" == "1" ]
 then
     IP_AT_CLOUDABOVE=$(dig A $DOMAIN +short @ns1.cloudabove.com)
-    RDNS_HOSTNAME=$(dig -x $IP_AT_CLOUDABOVE +short)
+    RDNS_HOSTNAME=$(dig -x $IP_AT_CLOUDABOVE +short | grep -v "^;")
+    CLOUDFLARE_NOTICE="\n${YELLOW}Note: This domain is behind CloudFlares Nameservers${NC}"
 else
     RDNS_HOSTNAME=$(dig -x $A_RECORD +short)
 fi
@@ -73,11 +80,7 @@ MAIL_RECORD=$(dig mail.$DOMAIN A +short | grep -v $DOMAIN)
 MX_RECORD=$(dig $DOMAIN MX +short)
 TXT_RECORD=$(dig $DOMAIN TXT +short)
 
-# Colours
-RED='\033[0;31m'
-YELLOW='\033[1;33m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+
 
 # Populates $NS with Nameserver Records depending on if its ICANN .com/.org etc. vs NOMINET .co.uk/.uk etc.
 if [ -z "$COM_NS_CHECK" ]
@@ -87,10 +90,10 @@ else
 	NS="$COM_NS_CHECK"
 fi
 
-echo "
+echo -e "
  -------------------------------------------------------------------------
 | Domain  :   $DOMAIN expires on $EXPIRY_DATE
- -------------------------------------------------------------------------
+ -------------------------------------------------------------------------$CLOUDFLARE_NOTICE
 | Nameservers:
 $NS
 -------------------------------------------------------------------------
